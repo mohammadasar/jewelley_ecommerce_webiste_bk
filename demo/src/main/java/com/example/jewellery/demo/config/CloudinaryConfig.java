b05c512d98f0bc4ab0c47ae5fe6f356a16da3adf
mohammadasar <itsmemdasar415@gmail.com>
2026-03-26 12:47:39 +0530

    modified cloudinaryconfig

--- demo/src/main/java/com/example/jewellery/demo/config/CloudinaryConfig.java
+++ demo/src/main/java/com/example/jewellery/demo/config/CloudinaryConfig.java
@@ -1,10 +1,10 @@
 package com.example.jewellery.demo.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
@@ -12,9 +12,9 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "YOUR_CLOUD_NAME",
-                "api_key", "YOUR_API_KEY",
-                "api_secret", "YOUR_API_SECRET"
+                "cloud_name", System.getenv("CLOUDINARY_CLOUD_NAME"),
+                "api_key", System.getenv("CLOUDINARY_API_KEY"),
+                "api_secret", System.getenv("CLOUDINARY_API_SECRET")
         ));
     }
 }
